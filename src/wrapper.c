e3b8253367efeb3f0327e6aeda13593d99f6f24f
Mike Spivey <mike@cs.ox.ac.uk>
2020-05-18 20:36:23 +0100

    Added configure script

--- src/wrapper.c
+++ src/wrapper.c
@@ -10,16 +10,10 @@ int main(int argc, char **argv) {
      char **args;
 
      if (argc > 2 && strcmp(argv[2], "--help") == 0) {
-          fprintf(stderr, "Help is at hand\n");
+          fprintf(stderr, "Help is at hand!\n");
           exit(1);
      }
 
-     args = calloc(argc+2, sizeof(char *));
-     args[0] = argv[0];
-     args[1] = "./xml2pmx.x";
-     for (int i = 1; i < argc; i++)
-          args[i+1] = argv[i];
-     args[argc+1] = NULL;
-     return xmain(argc+1, args);
+     return xmain(argc, argv);
 }
     
