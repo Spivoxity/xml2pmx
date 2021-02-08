5818b008d9b821cb060cf16a10933b921b8e92a2
Mike Spivey <mike@cs.ox.ac.uk>
2021-02-08 15:04:22 +0000

    Reinstated wrapper.c with --help and --version

--- wrapper.c
+++ wrapper.c
@@ -1,14 +1,23 @@
 /* wrapper.c -- Copyright (c) 2020 J. M. Spivey */
 
+#include "config.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+#undef main
+
 int xmain(int argc, char **argv);
 
 int main(int argc, char **argv) {
-     if (argc > 2 && strcmp(argv[2], "--help") == 0) {
+     if (argc >= 2 && strcmp(argv[1], "--help") == 0) {
           fprintf(stderr, "Help is at hand!\n");
+          exit(1);
+     }
+
+     if (argc >= 2 && strcmp(argv[1], "--version") == 0) {
+          fprintf(stderr, "%s version %s\n",
+                  PACKAGE_NAME, PACKAGE_VERSION);
           exit(1);
      }
 
